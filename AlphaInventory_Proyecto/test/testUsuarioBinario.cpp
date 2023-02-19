86ff068a3ab3cf7ed609cdff90d39150513af69a
QuantumRevenant <sebaperales14@gmail.com>
2023-02-19 13:42:10 -0500

    test

--- AlphaInventory_Proyecto/test/testUsuarioBinario.cpp
+++ AlphaInventory_Proyecto/test/testUsuarioBinario.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include "../controller/usuarioController.h"
 
-usuarioController userController;
+// usuarioController userController;
 
 using namespace std;
 
@@ -11,16 +11,43 @@ using namespace std;
 
 */
 
-int main(int argc, char *argv[])
+class test
 {
-    cout << "Here" << endl;
-    system("pause");
-    userController.archRecuperarDatos();
-    cout << "Here" << endl;
-    for (int i = 0; i < userController.size(); i++)
-        cout << userController.get(i).getCodUsuario() << endl;
-    cout<<userController.size();
-    cout << "Here" << endl;
-    userController.archGrabarDatos();
+public:
+    string nombre;
+    test() { nombre = "empty"; }
+    test(string _nombre) { nombre = _nombre; }
+};
+
+int main(int argc, char const *argv[])
+{
+    test a("1");
+    test b("2");
+    vector<test> vec = {a, b};
+    test obj2;
+
+    std::ofstream s("file.bin", std::ios::out|std::ios::app | std::ios::binary);
+    for (test x : vec)
+        s.write((const char *)&x, sizeof(x));
+    s.close();
+    std::ifstream in("file.bin", std::ios::in | std::ios::binary);
+    in.read((char *)&obj2, sizeof(obj2));
+    in.close();
+    std::cout << obj2.nombre << '\n';
+
     return 0;
 }
+
+// int main(int argc, char *argv[])
+// {
+// cout << "Here" << endl;
+// system("pause");
+// userController.archRecuperarDatos();
+// cout << "Here" << endl;
+// for (int i = 0; i < userController.size(); i++)
+//     cout << userController.get(i).getCodUsuario() << endl;
+// cout<<userController.size();
+// cout << "Here" << endl;
+// userController.archGrabarDatos();
+// return 0;
+// }
